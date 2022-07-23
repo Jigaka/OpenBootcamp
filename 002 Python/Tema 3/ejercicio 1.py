7071f8e5040241a00cf382454c563b79ac18d2cd
José Garcete <garcetejoseka@gmail.com>
2022-07-23 15:42:33 -0400

    Resolucion ejercicio1 Tema 3

---
+++ 002 Python/Tema 3/ejercicio 1.py
@@ -0,0 +1,19 @@
+nombre = "Nombre"
+apellidos = "Apellidos"
+edad = 20
+email = "email@email.com"
+telefono = "45678912"
+casado = False
+conHijos = False
+listaDeAmigos = ["Amigo1", "Amigo2", "Amigo3"]
+peliculasVistas = { "pelicula" : "Thor", "pelicula": "Mi villano favorito"}
+
+print(nombre)
+print(apellidos)
+print(edad)
+print(email)
+print(telefono)
+print(casado)
+print(conHijos)
+print(listaDeAmigos)
+print(peliculasVistas)
