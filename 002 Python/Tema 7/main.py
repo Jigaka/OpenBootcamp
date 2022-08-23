f3c9894f642fae9aaebee8c4ccea6bd2012b946f
Jigaka <garcetejoseka@gmail.com>
2022-08-23 18:37:36 -0400

    Resolucion ejercicio 1 tema 7

---
+++ 002 Python/Tema 7/main.py
@@ -0,0 +1,10 @@
+import operaciones as op
+
+def main():
+    print("La suma 5 + 4 es", op.suma(4,5))
+    print("La resta 5 - 4 es", op.resta(5,4))
+    print("La multiplicacion de 5 * 4 es", op.multiplica(4,5))
+    print("La division de 10 / 2 es", op.divide(10,2))
+
+if __name__ == "__main__":
+    main()
